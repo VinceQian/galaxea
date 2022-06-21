63a056622a8f40e3fafe157532a4fae5a0535969
VinceQian <qqian2@uw.edu>
2022-06-20 19:01:43 -0700

    v 0.1 initial settings.

--- app.r
+++ app.r
@@ -1,48 +1,13 @@
-library(shiny)
+# app.R
+# Authors: Kaia Truong, Aliya Ali, Randy Ros, Vince Qian
+# Final Deliverable INFO 201
 
-#Put the UI parts here
-ui <- fluidPage(
-  h1("Let's do a Survey!!!"),
-  p("Answer the following questions:"),
-  textInput(
-    inputId = "personName",
-    label = "What is your name?"
-  ),
-  textInput(
-    inputId = "favoriteGame",
-    label = "What is your favorite game?"
-  ),
-  radioButtons(
-    inputId = "game",
-    label = "Is the game a video game?",
-    choices = list("yes"= 1, "no" = 2),
-    selected = 1
-  ),
-  h1("Your Answers:"),
-  textOutput(outputId = "nameMessage"),
-  textOutput(outputId = "gameMessage"),
-  textOutput(outputId = "radioMessage")
-)
+# Load library
+library("shiny")
 
-#Server stuff goes here 
-server <- function(input, output){
-  output$nameMessage <- renderText({
-    paste("Hello!", input$personName)
-  })
-  
-  output$gameMessage <- renderText({
-    paste("Your favorite game is", input$favoriteGame)
-  })
-  
-  output$radioMessage <- renderText({
-    #paste("You selected option:", input$soup)
-    if (input$game == 1){
-      paste("It is a video game.")
-    } else{
-      paste("It is not a video game.")
-    }
-  })
-}
+# Source ui and server of the app
+source("ui.R")
+source("server.R")
 
-#part that actually makes the shiny app
+# Run the app
 shinyApp(ui = ui, server = server)
---
+++ server.R
@@ -0,0 +1,12 @@
+# server.R
+
+# Load libraries
+library(dplyr)
+library(shiny)
+library(plotly)
+library(ggplot2)
+
+# Define server function
+server <- function(input, output) {
+  
+}
