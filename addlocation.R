bca8b63c7834edea5ba48630bd7154c4f37de923
VinceQian <qqian2@uw.edu>
2022-06-24 12:38:59 -0700

    v 0.132 pic exp 3

--- addlocation.R
+++ addlocation.R
@@ -11,7 +11,7 @@ write.csv(locations,file = filename)
 
 #太古广场
 newlocation <- data.frame("2","Pacific Place / Downtown 太古广场/市中心",
-                       "2021-12-21","https://github.com/VinceQian/galaxea/blob/main/pic/IMG_3446.JPG?raw=true",
+                       "2021-12-21","https://github.com/VinceQian/galaxea/blob/main/pic/IMG_1174.HEIC?raw=true",
                        "圣诞前夕，冬日的冷雨，街道上的火树银花和广场内的节日氛围。",
                        "冬至日的海底捞大餐和餐后的小电影。",
                        "附近的电影院挺多，别买错票。海底捞唱生日歌时记得录视频。")
