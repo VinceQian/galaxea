9ebc1182718b7125c1ff2102513503363ca01538
VinceQian <qqian2@uw.edu>
2022-07-17 00:06:17 -0700

    v0.2 module.

--- server.R
+++ server.R
@@ -54,26 +54,26 @@ server <- function(input, output) {
       ),
       fluidRow(
         column(12,
-              h4("内容"),
-              paste(courses %>%
-                      filter(X == input$course) %>%
-                      pull(intro)),
-              h4("教授"),
-              paste(courses %>%
-                      filter(X == input$course) %>%
-                      pull(professor)),
-              h4("作业"),
-              paste(courses %>%
-                      filter(X == input$course) %>%
-                      pull(ass)),
-              h4("合作"),
-              paste(courses %>%
-                      filter(X == input$course) %>%
-                      pull(peer)),
-              h4("其他"),
-              paste(courses %>%
-                      filter(X == input$course) %>%
-                      pull(tip))
+               h4("内容"),
+               paste(courses %>%
+                       filter(X == input$course) %>%
+                       pull(intro)),
+               h4("教授"),
+               paste(courses %>%
+                       filter(X == input$course) %>%
+                       pull(professor)),
+               h4("作业"),
+               paste(courses %>%
+                       filter(X == input$course) %>%
+                       pull(ass)),
+               h4("合作"),
+               paste(courses %>%
+                       filter(X == input$course) %>%
+                       pull(peer)),
+               h4("其他"),
+               paste(courses %>%
+                       filter(X == input$course) %>%
+                       pull(tip))
         )
       )
     )
